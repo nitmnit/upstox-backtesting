4e7965d07f82b329bc95c0cb41828462b2a777ff
Nitin Suresh <nitin.kalal@pasarpolis.com>
2019-02-05 21:28:56 +0530

    created migrations for instruments model

--- zedi/freaks/models.py
+++ zedi/freaks/models.py
@@ -17,34 +17,6 @@ class Credential(TimeStampedModel):
     def __str__(self):
         return self.name
 
-    def set_access_token(self):
-        con = SeleniumConnector(logger=self.logger)
-        con.driver.get(self.con.login_url())
-        time.sleep(3)
-        username_input = con.driver.find_element_by_css_selector('input[type="text"]')
-        password_input = con.driver.find_element_by_css_selector('input[type="password"]')
-        submit_button = con.driver.find_element_by_css_selector('button[type="submit"]')
-        username_input.send_keys(settings.KITE['USERNAME'])
-        password_input.send_keys(settings.KITE['PASSWORD'])
-        submit_button.click()
-        time.sleep(5)
-        sq1_el = con.driver.find_element_by_css_selector('.twofa-form > div:nth-child(2) input')
-        sq2_el = con.driver.find_element_by_css_selector('.twofa-form > div:nth-child(3) input')
-        question1 = sq1_el.get_attribute('label')
-        question2 = sq2_el.get_attribute('label')
-        answer1 = self.get_answer(question1)
-        answer2 = self.get_answer(question2)
-        sq1_el.send_keys(answer1)
-        sq2_el.send_keys(answer2)
-        answer_submit_button = con.driver.find_element_by_css_selector('button[type="submit"]')
-        answer_submit_button.click()
-        time.sleep(3)
-        parsed = urlparse.urlparse(con.driver.current_url)
-        request_token = urlparse.parse_qs(parsed.query)['request_token'][0]
-        con.driver.close()
-        self.log('Got request token. {}'.format(request_token))
-
-
 
 class SecurityQuestion(TimeStampedModel):
     question = models.CharField(max_length=NAME_LENGTH * 10)
@@ -53,3 +25,21 @@ class SecurityQuestion(TimeStampedModel):
 
     def __str__(self):
         return self.question
+
+
+class Instrument(TimeStampedModel):
+    name = models.CharField(max_length=NAME_LENGTH)
+    instrument_token = models.IntegerField()
+    exchange_token = models.IntegerField()
+    trading_symbol = models.CharField(max_length=NAME_LENGTH)
+    last_price = models.FloatField(null=True, blank=True)
+    expiry = models.DateTimeField(null=True, blank=True)
+    strike = models.FloatField(null=True, blank=True)
+    tick_size = models.FloatField(null=True, blank=True)
+    lot_size = models.IntegerField()
+    instrument_type = models.CharField(max_length=NAME_LENGTH)
+    segment = models.CharField(max_length=NAME_LENGTH)
+    exchange = models.CharField(max_length=NAME_LENGTH)
+
+    def __str__(self):
+        return '{}-{}'.format(self.name, self.instrument_token)
